ede37182bf99b24569718b6e0a506e72c9f4d765
augcouec <augcouec@gmail.com>
2020-12-18 21:00:33 +0100

    fix package proxy and pics reducer

--- src/domain/picture/picture.reducer.js
+++ src/domain/picture/picture.reducer.js
@@ -1,34 +1,39 @@
-import { types } from './picture.actions';
+import { types } from "./picture.actions";
 
 export default function reducer(state, action) {
-    switch (action.type) {
-        case types.PICTURE_STARTED:
-            return {
-                ...state,
-                pending: true
-            }
-        case types.PICTURE_DONE:
-            return {
-                ...state,
-                pending: false,
-                pictures: action.payload
-            }
-        case types.PICTURE_LIKED:
-            const { pictures } = state;
-            const idx = pictures.findIndex(picture => picture.id === action.payload.id);
-            pictures[idx] = action.payload;
-            return {
-                ...state,
-                pending: false,
-                pictures: [...pictures]
-            }
-        case types.PICTURE_FAILED:
-            return {
-                ...state,
-                pending: false,
-                error: action.payload
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case types.PICTURE_STARTED:
+      return {
+        ...state,
+        pending: true,
+      };
+    case types.PICTURE_DONE:
+      return {
+        ...state,
+        pending: false,
+        pictures: action.payload,
+      };
+    case types.PICTURE_LIKED:
+      const { pictures, user } = state;
+      const idx = pictures.findIndex(
+        (picture) => picture._id === action.payload._id
+      );
+      pictures[idx] = {
+        ...pictures[idx],
+        likedBy: [...pictures[idx].likedBy, user._id],
+      };
+      return {
+        ...state,
+        pending: false,
+        pictures: [...pictures],
+      };
+    case types.PICTURE_FAILED:
+      return {
+        ...state,
+        pending: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
 }
